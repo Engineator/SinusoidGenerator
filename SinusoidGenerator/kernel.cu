fb058151684fc4a3cbd71c06c8dd9a56aa27a784
Jeffrey Nybo <jeffmnybo@gmail.com>
2020-05-21 18:03:41 -0600

    Makes Wav Files

--- SinusoidGenerator/kernel.cu
+++ SinusoidGenerator/kernel.cu
@@ -1,36 +1,40 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "wavfile.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <math_constants.h>
 
+
+
 #define FREQUENCY 1000.0 // Frequency in Hz
 #define SAMPLE_RATE 44100.0
 #define N_SECONDS 4.0
-#define N_SAMPLES (N_SECONDS * SAMPLE_RATE)
+//#define N_SAMPLES (N_SECONDS * SAMPLE_RATE)
+#define N_SAMPLES (WAVFILE_SAMPLES_PER_SECOND*2)
 
 #define BLOCK_SIZE 1024
 
-cudaError_t cudaSinusoidGenerator(double *signal, unsigned int size);
+cudaError_t cudaSinusoidGenerator(short *signal, unsigned int size);
 
-__global__ void sinusoidGeneratorKernel(double *signal)
+__global__ void sinusoidGeneratorKernel(short *signal)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    signal[i] = sin(2*CUDART_PI*FREQUENCY*((double)i/SAMPLE_RATE)); 
+    signal[i] = 10000*sin(2*CUDART_PI*FREQUENCY*((short)i/SAMPLE_RATE)); 
     //signal[i] = (double)i;
 }
 
 int main()
 {
 
-    double * signal = (double *)malloc(N_SAMPLES * sizeof(double));
+    short * signal = (short *)malloc(N_SAMPLES * sizeof(short));
 
     for (int i = 0; i < N_SAMPLES; i++)
     {
-        signal[i] = (double)i;
+        signal[i] = (short)i;
     }
 
     //// Generate Sinusoid
@@ -43,15 +47,19 @@ int main()
     FILE* outFile;
 
 
-    outFile = fopen("signal_out.txt", "w+");
-    for (int i = 0; i < N_SAMPLES; i++)
+    //outFile = fopen("signal_out.txt", "w+");
+    /*for (int i = 0; i < N_SAMPLES; i++)
     {
         fprintf(outFile, "%f\n", signal[i]);
     }
     fclose(outFile);
 
-    free(signal);
-    
+    free(signal);*/
+
+    int length = N_SAMPLES;
+
+    outFile = wavfile_open("signal_sound.wav");
+    wavfile_write(outFile, signal, length);
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -65,9 +73,9 @@ int main()
 }
 
  // Helper function for using CUDA to generate a sinusoid
-cudaError_t cudaSinusoidGenerator(double *signal, unsigned int size)
+cudaError_t cudaSinusoidGenerator(short *signal, unsigned int size)
 {
-    double *dev_signal = 0;
+    short *dev_signal = 0;
     cudaError_t cudaStatus;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -78,7 +86,7 @@ cudaError_t cudaSinusoidGenerator(double *signal, unsigned int size)
     }
 
     // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_signal, size * sizeof(double));
+    cudaStatus = cudaMalloc((void**)&dev_signal, size * sizeof(short));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
@@ -105,7 +113,7 @@ cudaError_t cudaSinusoidGenerator(double *signal, unsigned int size)
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(signal, dev_signal, size * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(signal, dev_signal, size * sizeof(short), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
